a89f6689c087a09a6de5b933091801a3c612a027
slabwu <134193743+slabwu@users.noreply.github.com>
2025-01-05 12:15:41 +0800

    Add recursive buildTree() method

--- bst.js
+++ bst.js
@@ -2,6 +2,22 @@ export class Tree {
     constructor() {
         this.root;
     }
+
+    buildTree(array, start = 0, end = array.length - 1) {
+        if (start > end) return null;
+
+        let mid = Math.floor((start + end) / 2);
+        let root = new Node(array[mid]);
+
+        root.left =this.buildTree(array, start, mid - 1);
+        root.right = this.buildTree(array, mid + 1, end);
+
+        return root;
+    }
+
+    create(array) {
+        this.root = this.buildTree(array);
+    }
 }
 
 class Node {
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import { Tree } from './bst.js';
 
 let tree = new Tree();
+
+tree.create([1, 2, 3]);
