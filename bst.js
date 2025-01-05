bfc671bba008e632f485f6fe494a9faa32009b00
slabwu <134193743+slabwu@users.noreply.github.com>
2025-01-05 12:03:42 +0800

    Create Node class

--- bst.js
+++ bst.js
@@ -1,3 +1,12 @@
 export function test() {
     console.log('Works!');
 }
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
