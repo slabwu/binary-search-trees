ac8d57d50907118ac72adc98e517815a7fd9bbd1
slabwu <134193743+slabwu@users.noreply.github.com>
2025-07-10 18:25:58 +0800

    Implement node deletion with delete()

--- bst.js
+++ bst.js
@@ -51,6 +51,38 @@ export class Tree {
         
         return root;
     }
+
+    delete(data, root = this.root) {
+        if (!root) return root;
+        if (root.data === data) {
+            if (!root.left) {
+                return root.right;
+            } else if (!root.right) {
+                return root.left;
+            } else {
+                let replacement = this.getReplacement(root);
+                root.data = replacement.data;
+                root.right = this.delete(replacement.data, root.right);
+                return root;
+            }
+        }
+      
+        if (root.data < data) {
+            root.right = this.delete(data, root.right);
+        } else {
+            root.left = this.delete(data, root.left);
+        }
+        
+        return root;
+    }
+
+    getReplacement(root) {
+        let tmp = root.right;
+        while (tmp.left) {
+            tmp = tmp.left;
+        }
+        return tmp;
+    }
 }
 
 class Node {
